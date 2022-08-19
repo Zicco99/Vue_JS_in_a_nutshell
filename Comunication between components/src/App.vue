965d2989536bf77f71bb6610ec45fab53e1be6fe
Francesco Giuseppe Ziccolella <52136996+Zicco99@users.noreply.github.com>
2022-08-19 17:21:22 +0200

    Add files via upload

---
+++ Comunication between components/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h2>My Friends</h2>
+  <p> Counter {{ counter }}</p>
+  <ul>
+    <friend-contact
+      v-for="friend in friends"
+      :key="friend.id"
+      :name="friend.name"
+      :phone-number="friend.phone"
+      :email-address="friend.email"
+      @increm="foo"
+    ></friend-contact>
+  </ul>
+
+  <!-- @(kebab case event)="foo" -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234567890",
+          email: "dio@gmail.com",
+        },
+        {
+          id: "juan",
+          name: "Juan 3Cozz",
+          phone: "01234567890",
+          email: "diomerda@gmail.com",
+        },
+      ],
+      counter: 0,
+    };
+
+  },
+  methods: {
+    foo(intero) {
+      this.counter+=intero;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ Comunication between components/src/component/FriendContact.vue
@@ -0,0 +1,64 @@
+<template>
+  <li>
+    <h2>{{ this.name }}</h2>
+    <button @click="inc">Increment</button>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisibile">
+      <li>Phone: {{ this.phoneNumber }}</li>
+      <li>Email: {{ this.emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      /* validator: function (value) {
+        return value === "1" || value === "0";
+      }, */
+    },
+  },
+  emits: {
+    increm: function (intero) {
+      if (intero > 0) {
+        return true;
+      } else {
+        alert("lol")
+        return false;
+      }
+    },
+  },
+  data() {
+    return {
+      detailsAreVisibile: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234567890",
+        email: "dio@gmail.com",
+      },
+    };
+  },
+  methods: {
+    inc() {
+      this.$emit("increm", 4);
+    },
+  },
+};
+</script>
